e959c17bbb50c47011e71e63d79df5e128087f11
oseymour <45520489+oseymour@users.noreply.github.com>
2021-03-26 01:24:15 -0500

    0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setuptools.setup(
     name = 'ScraperFC',
-    version = '0.0.7',
+    version = '0.0.8',
     author = 'Owen Seymour',
     author_email = 'osmour043@gmail.com',
     description = 'Package for scraping soccer data from a variety of sources.',
