597c91a4943d1912d9d7e7e5543199f521150246
oseymour <45520489+oseymour@users.noreply.github.com>
2021-03-29 17:21:25 -0500

    still can't get it working on other computer

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setuptools.setup(
     name = 'ScraperFC',
-    version = '0.0.8',
+    version = '0.1.8',
     author = 'Owen Seymour',
     author_email = 'osmour043@gmail.com',
     description = 'Package for scraping soccer data from a variety of sources.',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/oseymour/ScraperFC',
-    packages = setuptools.find_packages(include=['ScraperFC', 'ScraperFC.*']),
-#     py_modules=['FBRef', 'FiveThirtyEight','ScraperFC','shared_functions','Understat'],
+    packages = setuptools.find_packages(),#include=['ScraperFC', 'ScraperFC.*']),
+    py_modules=['FBRef','FiveThirtyEight','Main','shared_functions','Understat'],
     keywords = ["soccer","football","Premier League","Serie A",
                 "La Liga","Bundesliga","Ligue 1"],
     classifiers = [
