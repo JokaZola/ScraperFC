10cb51cc932184d803d2be78817378527c666b96
oseymour <45520489+oseymour@users.noreply.github.com>
2021-04-04 01:29:22 -0500

    mainly understat
    Understat can now scrape match data from all of the big 5 leagues
    
    Also updated check_season in shared_functions.py. Now checks the league and ythe ear

--- code/ScraperFC/Understat.py
+++ code/ScraperFC/Understat.py
@@ -24,26 +24,81 @@ class Understat:
         self.driver.quit()
         
         
-    def get_match_links(self, year):
-        
-        base_url = 'https://understat.com/match/'
-        starts = {
-            2020: 11643,
-            2019: 9197,
-            2018: 7119,
-            2017: 461,
-            2016: 81,
-            2015: 4749
-        }
-        start_id = starts[year]
+    def get_match_links(self, year, league):
+        if not check_season(year,league,'Understat'):
+            return -1
+#         base_url = 'https://understat.com/match/'
+#         starts = {
+#             # EPL
+#             ("EPL",2015): 4749,
+#             ("EPL",2016): 81,
+#             ("EPL",2017): 461,
+#             ("EPL",2018): 7119,
+#             ("EPL",2019): 9197,
+#             ("EPL",2020): 11643,
+#             ("EPL",2021): 14086,
+#             # La Liga
+#             ("La Liga",2015): 5826,
+#             ("La Liga",2016): 1399,
+#             ("La Liga",2017): 1779,
+#             ("La Liga",2018): 7879,
+#             ("La Liga",2019): 9957,
+#             ("La Liga",2020): 12026,
+#             ("La Liga",2021): 14136,
+#             # Bundesliga
+#             ("Bundesliga",2015): 5447,
+#             ("Bundesliga",2016): 1021,
+#             ("Bundesliga",2017): 1327,
+#             ("Bundesliga",2018): 8259,
+#             ("Bundesliga",2019): 10337,
+#             ("Bundesliga",2020): 12403,
+#             ("Bundesliga",2021): 14173,
+#             # Serie A
+#             ("Serie A",2015): 5149,
+#             ("Serie A",2016): 551,
+#             ("Serie A",2017): 931,
+#             ("Serie A",2018): 7499,
+#             ("Serie A",2019): 9577,
+#             ("Serie A",2020): 13089,
+#             ("Serie A",2021): 14116,
+#             # Ligue 1
+#             ("Ligue 1",2015): 6185,
+#             ("Ligue 1",2016): 1869,
+#             ("Ligue 1",2017): 2249,
+#             ("Ligue 1",2018): 8565,
+#             ("Ligue 1",2019): 10643,
+#             ("Ligue 1",2020): 12709,
+#             ("Ligue 1",2021): 13977
+#         }
+#         start_id = starts[(league,year)]
+#         if league in ["EPL","La Liga","Serie A","Ligue 1"]:
+#             games_in_season = 380
+#         elif league == "Bundesliga":
+#             games_in_season = 306
+#         links = []
+#         for id in range(start_id, start_id+games_in_season):
+#             links.append(base_url+str(id))
+        base_url = "https://understat.com/"
+        lg = league.replace(" ","_")
+        url = base_url+"league/"+lg+"/"+str(year-1)
+        self.driver.get(url)
+        btn = self.driver.find_element_by_class_name("calendar-prev")
         links = []
-        for id in range(start_id, start_id+380):
-            links.append(base_url+str(id))
+        for el in self.driver.find_elements_by_class_name("match-info"):
+            links.append(el.get_attribute("href"))
+        done = False
+        while not done:
+            btn.click()
+            for el in self.driver.find_elements_by_class_name("match-info"):
+                href = el.get_attribute("href")
+                if href not in links:
+                    links.append(href)
+                else:
+                    done = True
         return links
         
         
     def scrape_match(self, link):
-        
         self.driver.get(link)
         elements = []
         for element in self.driver.find_elements_by_class_name('progress-value'):
@@ -60,7 +115,10 @@ class Understat:
         match['Away Team'] = elements[1]
         
         for element in self.driver.find_elements_by_class_name('block-match-result'):
-            score = [i for i in element.text if i.isdigit()]
+             score = element.get_attribute("innerHTML")
+             score = score.split("</a>")[1]
+             score = score.split("<a")[0].strip()
+             score = score.split(" - ")
         assert len(score) == 2
         match['Home Goals'] = int(score[0])
         match['Away Goals'] = int(score[1])
@@ -95,26 +153,24 @@ class Understat:
         return match
         
         
-    def scrape_matches(self, year, save=False):
-        
+    def scrape_matches(self, year, league, save=False):
         if not check_season(year,'EPL','Understat'):
             return -1
         
         season = str(year-1)+'-'+str(year)
-        links = self.get_match_links(year)
+        links = self.get_match_links(year, league)
         cols = ['Date','Home Team','Away Team','Home Goals','Away Goals',
                 'Home Ast','Away Ast','Understat Home xG','Understat Away xG',
                 'Understat Home xA','Understat Away xA','Home xPts','Away xPts']
         matches = pd.DataFrame(columns=cols)
         
         for i,link in enumerate(links):
-            
             print('Scraping match ' + str(i+1) + '/' + str(len(links)) + ' from Understat in the ' + season + ' season.')
             print(link)
             match = self.scrape_match(link)
             matches = matches.append(match, ignore_index=True)
             clear_output()
-            
+        
         # save to CSV if requested by user
         if save:
             filename = 'EPL_matches_'+season+'_Understat.csv'
