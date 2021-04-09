4e3e8ad8e3fb468e87cd7d2ddafd79dec6488b10
oseymour <45520489+oseymour@users.noreply.github.com>
2021-04-08 23:32:59 -0500

    Understat can scrape league tables now

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setuptools.setup(
     name = 'ScraperFC',
-    version = '0.1.16',
+    version = '0.1.17',
     author = 'Owen Seymour',
     author_email = 'osmour043@gmail.com',
     description = 'Package for scraping soccer data from a variety of sources.',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/oseymour/ScraperFC',
-#     packages = setuptools.find_packages(),#include=['ScraperFC', 'ScraperFC.*']),
-#     py_modules=['FBRef','FiveThirtyEight','Main','shared_functions','Understat'],
     package_dir = {"":"code"},
     packages = setuptools.find_packages(where="code"),
     keywords = ["soccer","football","Premier League","Serie A",
