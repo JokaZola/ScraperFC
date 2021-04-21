5e3c804a2850ad1270e40c0b8e92ec9aafcd8370
oseymour <45520489+oseymour@users.noreply.github.com>
2021-04-21 00:10:30 -0500

    Finished player stats for FBRef

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setuptools.setup(
     name = 'ScraperFC',
-    version = '1.0.1',
+    version = '1.1.0',
     author = 'Owen Seymour',
     author_email = 'osmour043@gmail.com',
     description = 'Package for scraping soccer data from a variety of sources.',
