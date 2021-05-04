7d080deda8fa6381dabd92149201125e32a8b958
oseymour <45520489+oseymour@users.noreply.github.com>
2021-05-03 21:48:44 -0500

    Removed time package dependency from setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setuptools.setup(
     name = 'ScraperFC',
-    version = '1.3.0',
+    version = '1.3.1',
     author = 'Owen Seymour',
     author_email = 'osmour043@gmail.com',
     description = 'Package for scraping soccer data from a variety of sources.',
@@ -21,7 +21,6 @@ setuptools.setup(
         "webdriver-manager",
         "pandas", "numpy",
         "datetime",
-        'time',
         "ipython"
     ],
     classifiers = [ 
